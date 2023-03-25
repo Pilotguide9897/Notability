b85d3dc80ce0ec3970af99ca0c47d82eaecebdea
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-03-25 12:45:51 -0300

    routes and methods

--- Develop/server.js
+++ Develop/server.js
@@ -2,27 +2,69 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const notes = require('./db/db.json');
+const fs = require('fs');
+const uuid = require('uuid');
 const PORT = process.env.PORT || 5001;
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.json());
 
-//HTML routes
-    // Route for returning the notes.html file
-    app.get('/notes', function(req, res) {
+// HTML routes
+    //Route for returning the notes.html file.
+    app.get('/notes', (req, res) => {
         res.sendFile(__dirname + '/public/notes.html');
     });
 
-    // Route for 
-    app.get('*', function(req,res){
+    // Route for returning the intex.html file.
+    app.get('*', (req,res) => {
         res.sendFile(__dirname + '/public/index.html');
     });
 
+  
+
 // API routes
-    app.get();
+    // Route to fetch note data.
+    app.get('/api/notes', (req,res) => {
+        const filePath = path.join(__dirname,'db', 'db.json')
+        fs.readFile(filePath, function(error,data) {
+            if (error) {
+                console.error(error);
+                res.status(500).send('An error occurred while fetching the notes.');
+            } else {
+                const notes = JSON.parse(data);
+                res.json(notes);
+            }
+        });
+    });
 
-    app.post();
+    // Route to add new notes to the db.json file.
+    app.post('/api/notes', (req, res) => {
+        const id = uuid.v4();
+        const newNote = { ...req.body, id }; // use the spread operator to create a new object that includes all the properties of the original req.body.
+        const filePath = path.join(__dirname,'db', 'db.json');
+        // First read the existing db.json file to get the previous notes.
+        fs.readFile(filePath, function(error, data){
+            if (error) {
+                console.error(error);
+                res.status(500).send('An error occurred while fetching the notes.');
+            } else {
+                const notes = JSON.parse(data);
+                notes.push(newNote);
+                // how do I add the unique uuid to this?
+            
+        fs.writeFile(filePath, /*need to re-stringify the data*/ JSON.stringify(notes), function(error) {
+            if (error) {
+                console.error(error);
+                res.status(500).send('An error occurred while saving your note...')
+            } else {
+                res.status(201).send('Note successfully saved!');
+            }
+        });
+        }
+    });
+});
 
-// Listens for connection.
+// Listens for connection. // Working.
 app.listen(PORT, () => {
     console.log(`Note Taker listening at http://localhost:${PORT}`);
 });
